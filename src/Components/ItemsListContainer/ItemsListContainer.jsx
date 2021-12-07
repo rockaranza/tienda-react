a856394552f1960307c19476fe473ee65b3e4c27
Diego Ocaranza <diego.ocaranza@outlook.com>
2021-12-07 14:48:39 -0300

    Desafio componentes II. Hice el componente ItemCount que aumenta el numero hasta 10 y no puede bajar de 1

--- src/Components/ItemsListContainer/ItemsListContainer.jsx
+++ src/Components/ItemsListContainer/ItemsListContainer.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import ItemCount from '../ItemCount/ItemCount';
 
 const ItemsListContainer = props => {
     return (
-        <div style={{display: 'flex', justifyContent: 'center', paddingTop: '3rem'}}>
+        <div style={{display: 'flex', flexDirection:'column',justifyContent: 'center', alignItems:'center',paddingTop: '3rem'}}>
             <h2>{props.greeting}</h2>
+            <ItemCount iniStock={1} maxStock={10}/>
         </div>
     )
 }
