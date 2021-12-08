1877d660d8bba10ce33de288fb0960b284a4f605
Diego Ocaranza <diego.ocaranza@outlook.com>
2021-12-08 19:31:26 -0300

    Desafio Promise y Map. Agregue componentes ItemList y Item, ademas se agrego carpeta Helpers con los productos y la promesa.

--- src/Components/ItemsListContainer/ItemsListContainer.jsx
+++ src/Components/ItemsListContainer/ItemsListContainer.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
-import ItemCount from '../ItemCount/ItemCount';
+import React, { useState, useEffect } from 'react';
+import { getFetch } from '../../Helpers/getFetch';
+import ItemList from '../ItemList/ItemList';
+// import ItemCount f rom '../ItemCount/ItemCount';
+
 
 const ItemsListContainer = props => {
+
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(() => {
+        getFetch
+            .then(resp => setProducts(resp))
+            .catch(err => console.log(err))
+            .finally(() => setLoading(false))
+    }, []);
+
     return (
         <div style={{display: 'flex', flexDirection:'column',justifyContent: 'center', alignItems:'center',paddingTop: '3rem'}}>
             <h2>{props.greeting}</h2>
-            <ItemCount iniStock={1} maxStock={10}/>
+            { loading ?
+                <h2>Cargando...</h2>
+                :
+                <ItemList products={products}/>
+
+            }
+            {/* <ItemCount iniStock={1} maxStock={10}/> */}
         </div>
     )
 }
